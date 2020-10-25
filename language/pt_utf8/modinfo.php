b3eeabbca4fa7e3a6da247b4485b95a233983d2a
mambax7 <mambax7@gmail.com>
2020-10-25 03:38:47 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ language/pt_utf8/modinfo.php
@@ -0,0 +1,14 @@
+<?php
+// Translation Info
+// *************************************************************** //
+// ############################################################### //
+// ## XOOPS Cube Legacy 2.1 - Tradução para o Português do Brasil
+// ############################################################### //
+// ## Por............: Mikhail Miguel
+// ## E-mail.........: mikhail@underpop.com
+// ## Website........: http://xoops.eti.br
+// ############################################################### //
+// *************************************************************** //
+define('_MI_STDCACHE_NAME', 'Acelerador');
+define('_MI_STDCACHE_NAME_DESC', 'Permite acelerar o portal via cache. Traduzido por Mikhail Miguel.');
+define('_MI_STDCACHE_BLOCK_CACHECLEAR', 'Limpeza do cache');
